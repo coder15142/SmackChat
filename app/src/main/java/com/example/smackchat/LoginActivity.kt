2ec9fe74c6a0682b3f41ac9349e32c86560a8dd6
Tejas <tejassiddesh@gmail.com>
2020-07-26 14:07:04 +0530

    initial commit

---
+++ app/src/main/java/com/example/smackchat/LoginActivity.kt
@@ -0,0 +1,30 @@
+package com.example.smackchat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.smackchat.R
+
+
+    class LoginActivity : AppCompatActivity() {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_login)
+        }
+
+
+        fun loginLoginBtnClicked(view: View){
+
+        }
+
+        fun loginSignUpBtnClicked(view: View){
+            val createUserIntent = Intent(this, CreateUserActivity::class.java)
+            startActivity(createUserIntent)
+        }
+
+
+    }
+
+
